654bac5641eb3cba3e307e9816ec5bdceafe93bf
memedev365 <memedev365@outlook.com>
2025-04-21 16:49:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // CORS setup
 const corsOptions = {
-  origin: ['http://localhost:3000'], // your frontend domain
+  origin: ['https://sshib-fe.vercel.app'], // your frontend domain
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // if your frontend needs cookies or auth
@@ -84,7 +84,7 @@ app.get('/api/events', (req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://sshib-fe.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
